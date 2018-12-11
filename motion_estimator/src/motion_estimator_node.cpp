bb5470da7ced768a746634361d366448d0f85c77
SubMishMar <subodheenitr@gmail.com>
2018-12-11 02:59:58 -0600

    WIP

--- motion_estimator/src/motion_estimator_node.cpp
+++ motion_estimator/src/motion_estimator_node.cpp
@@ -7,12 +7,15 @@ private:
     ros::NodeHandle nh;
     ros::Subscriber subLaserCloud;
 
+    pcl::PointCloud<PointType>::Ptr laserCloudIn_1;
     pcl::PointCloud<PointType>::Ptr laserCloudIn;
 
     std_msgs::Header cloudHeader;
 
     bool first_time;
 
+    tf::TransformListener listener;
+    tf::StampedTransform transform;
 public:
     MotionEstimator():
         nh("~"){
@@ -20,23 +23,60 @@ public:
         subLaserCloud = nh.subscribe<sensor_msgs::PointCloud2>("/velodyne_points", 1, &MotionEstimator::cloudHandler, this);
         first_time = true;
         allocateMemory();
-        resetParameters();
     }
 
     void allocateMemory(){
+        laserCloudIn_1.reset(new pcl::PointCloud<PointType>());
         laserCloudIn.reset(new pcl::PointCloud<PointType>());
     }
 
-    void resetParameters(){
+    void resetVariables(){
+        laserCloudIn_1->clear();
+        *laserCloudIn_1 = *laserCloudIn;
         laserCloudIn->clear();
     }
 
     ~MotionEstimator(){}
 
-    void cloudHandler(const sensor_msgs::PointCloud2ConstPtr& laserCloudMsg){
+    void getInitTransform() {
+        try{
+          ros::Time now = ros::Time::now();
+          listener.waitForTransform("/world", "/init_frame",
+                                    now, ros::Duration(1.0));
+          listener.lookupTransform("/world", "/init_frame",  
+                                    now, transform);
+          tf::Vector3 origin = transform.getOrigin();
+          tf::Quaternion rotn = transform.getRotation();
+        }
+        catch (tf::TransformException ex){
+          ROS_ERROR("%s",ex.what());
+          ros::Duration(1.0).sleep();
+        }
+    }
+
+    void runICP() {
+        pcl::IterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ> icp;
+        icp.setInputSource(laserCloudIn_1);
+        icp.setInputTarget(laserCloudIn);
+        pcl::PointCloud<pcl::PointXYZ> Final;
+        icp.align(Final);
+        std::cout << "ICP has converged:" << icp.hasConverged() << " score: " << icp.getFitnessScore() << std::endl;
+        std::cout << icp.getFinalTransformation() << std::endl;
+    }
+
+    void cloudHandler(const sensor_msgs::PointCloud2ConstPtr& laserCloudMsg){  
         cloudHeader = laserCloudMsg->header;
-        pcl::fromROSMsg(*laserCloudMsg, *laserCloudIn);
-        resetParameters();
+        if(first_time) {
+            first_time = false;
+            pcl::fromROSMsg(*laserCloudMsg, *laserCloudIn_1);
+            getInitTransform();         
+        } else {
+            pcl::fromROSMsg(*laserCloudMsg, *laserCloudIn);
+            std::cout << laserCloudIn_1->points.size() << "\t" << laserCloudIn->points.size() << std::endl;
+            runICP();
+            resetVariables();
+        }
+        
     }
 };
 
